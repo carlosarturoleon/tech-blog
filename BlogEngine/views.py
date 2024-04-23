0405d2134ac1ab96ef1496c1abc4779104257807
carlosarturoleon <ctr486@yahoo.com>
2024-04-22 21:40:06 -0500

    Adding dinamic breadcrumbs

--- BlogEngine/views.py
+++ BlogEngine/views.py
@@ -6,19 +6,62 @@ def index(request):
     return render(request, 'index.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    post = 'Contact Form'
+    breadcrumbs = [
+        ('index', 'Homepage'),
+        ('contact', 'Contact Form')
+    ]
+
+    context = {
+        'post': post,
+        'breadcrumbs': breadcrumbs
+    }
+    return render(request, 'contact.html', context)
 
 def topics_detail(request):
     return render(request, 'topics-detail.html')
 
 def topics_listing(request):
-    return render(request, 'topics-listing.html')
+    post = 'Topic Listing'
+    breadcrumbs = [
+        ('index', 'Homepage'),
+        ('topics_listing', 'Topic Listing')
+    ]
+
+    context = {
+        'post': post,
+        'breadcrumbs': breadcrumbs
+    }
+    return render(request, 'topics-listing.html', context)
 
 def posts_listing(request):
     posts = BlogPost.objects.filter(live=True).order_by('-updated_at')
-    return render(request, 'posts-list.html', {'posts': posts})
+    post = 'Blog Posts'
+    breadcrumbs = [
+        ('index', 'Homepage'),
+        ('contact', 'Contact Form')
+    ]
+
+    context = {
+        'posts': posts,
+        'post': post,
+        'breadcrumbs': breadcrumbs
+    }
+    return render(request, 'posts-list.html', context)
 
 def post_detail(request, slug):
     post = get_object_or_404(BlogPost, slug=slug, live=True)  # Ensure the post is live
-    return render(request, 'post-detail.html', {'post': post})
+
+    # Dynamic breadcrumbs setup
+    breadcrumbs = [
+        ('index', 'Homepage'),  # Static homepage breadcrumb
+        ('post', 'Posts'),
+        ('post_detail', post.title)  # Dynamic breadcrumb based on the post's title
+    ]
+
+    context = {
+        'post': post,
+        'breadcrumbs': breadcrumbs  # Include the breadcrumbs in the context
+    }
+    return render(request, 'post-detail.html', context)
 
